ffb0a1f156cbc01a84634b49d6b7d635dee77137
Penton, Ron <Ron.Penton@ellucian.com>
2017-08-18 19:36:38 -0400

    modularizing game data

---
+++ src/data/defaultGame.js
@@ -0,0 +1,109 @@
+export const game = {
+    
+    name: 'le Sim Game',
+    updateGranularity: 0.25,
+    maxUpdateCycles: 3600,
+    tabs: [
+        {
+            title: 'Gold & stuff',
+            items: [
+                {
+                    title: 'Gold',
+                    primary: 'Gold',
+                    items: [
+                        { items: ['Gold'] },
+                        { items: ['Gold Mine', 'Max Gold Mines'] },
+                        { primary: 'Gold Prospector' },
+                    ]
+                },
+                { title: 'Empty Group', items: [] },
+                { title: 'Fecal Matter', items: [{ primary: 'Poop' }] },
+            ]
+        },
+        {
+            title: 'Remix',
+            primary: 'Poop',
+            items: [
+                { title: 'Feces', primary: 'Poop', items: [] },
+                { title: 'Not Feces', primary: 'Gold Mine', items: [{ primary: 'Gold Mine' }] },
+            ]
+        },
+        { title: 'Surprise third tab!', primary: 'Gold Prospector' },
+    ],
+    growths: {
+        '+100%': {
+            growthFunction: 'percentage',
+            coefficient: 100,
+            tier: 0,
+        },
+        '*1.1': {
+            growthFunction: 'multiplicative',
+            coefficient: 1.1,
+            tier: 0,
+        },
+        '*2': {
+            growthFunction: 'multiplicative',
+            coefficient: 2,
+            tier: 0,
+        },
+        '-40%': {
+            growthFunction: 'percentage',
+            coefficient: -40,
+            tier: 1,
+        },
+    },
+    resources: {
+        Gold: {
+            description: "This is your good old boring gold. Basic unit for spending etc etc",
+            locked: false,
+            count: 10000,
+            price: [],
+        },
+        'Gold Mine': {
+            description: "This is an entire fucking gold mine that could be yours",
+            locked: false,
+            count: 1,
+            price: [{ entity: 'Gold', baseRate: 5 }],
+            sellModifier: ['-40%'],
+            purchaseGrowth: ['+100%'],
+            outputRate: [{ entity: 'Gold', baseRate: 1 }],
+        },
+        'Max Gold Mines': {
+            description: "Increases the maximum number of gold mines you can have",
+            visible: false,
+            locked: false,
+            requirement: [{ entity: 'Gold Mine', baseRate: 5 }],
+            price: [{ entity: 'Gold', baseRate: 100 }],
+            purchaseGrowth: ['*2'],
+            setsLimitFor: [
+                {
+                    entity: 'Gold Mine',
+                    bonuses: [
+                        { growthFunction: 'exponential', coefficient: 2 },
+                        { growthFunction: 'multiplicative', coefficient: 5 },
+                    ]
+                }],
+        },
+        'Gold Prospector': {
+            description: "Finds gold mines for you. Kinda fucking weird if you think about it",
+            visible: false,
+            requirement: [{ entity: 'Gold Mine', baseRate: 35 }],
+            price: [
+                { entity: 'Gold', baseRate: 1000 },
+                { entity: 'Gold Mine', baseRate: 10 }],
+            sellModifier: ['-40%'],
+            purchaseGrowth: ['*1.1'],
+            inputRate: [{ entity: 'Gold', baseRate: 50 }],
+            outputRate: [{ entity: 'Gold Mine', baseRate: 1 }],
+        },
+        Poop: {
+            description: "This is literally shit",
+            locked: false,
+            price: [{ entity: 'Gold', baseRate: 10 }],
+            purchaseGrowth: ['*2'],
+            inputRate: [{ entity: 'Gold', baseRate: 5 }],
+        },
+    },
+}
+
+export default game;
