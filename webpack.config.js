b0388c57823d79114c4be2eb8c471cbc8e08d05d
pouya.larjani <pouya.larjani@gmail.com>
2017-08-18 02:48:45 -0400

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  
+  entry: {
+    'bundle': './src/index.jsx',
+  },
+
+  output: {
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  
+  resolve: {
+    extensions: ['.jsx', '.js', ".css"],
+  },
+
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      include: /src/,
+      exclude: /node_modules/,
+      query: {
+        presets: ['env', 'react'],
+      }
+    }]
+  },
+
+  plugins: [
+  ],
+
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+  },
+};
