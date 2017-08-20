1c8f0473b9ad71addda2e9311f6bbe07fb77f536
lepouya <pouya.larjani@gmail.com>
2017-08-20 03:46:52 -0400

    Moving components from ui/ to components/

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import saveGame from './data/defaultGame';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ResourceManager from "./model/ResourceManager";
-import Game from './ui/Game';
+import Game from './components/Game';
 
 window.onload = event => ReactDOM.render(
   <Game resourceManager={new ResourceManager().load(saveGame)} />,
