5949d31dfb7cdda584d6a51d0d789cff199c72e8
lepouya <pouya.larjani@gmail.com>
2017-08-24 00:48:53 -0400

    Adding a reset button to DebugInfo screen

--- src/components/DebugInfo.jsx
+++ src/components/DebugInfo.jsx
@@ -2,9 +2,15 @@ import '../styles/debugInfo';
 import React from 'react';
 
 export default class DebugInfo extends React.Component {
+  reset() {
+    this.props.resourceManager.resetLocalStorage();
+    window.location.reload(false);
+  }
+
   render() {
     return <div id='debugInfo'>
       <hr />
+      <a onClick={_ => this.reset()} href='#'>Reset Game</a><br/>
       Debug info:<br/>
       Last update: {new Date(this.props.resourceManager.timeStamp * 1000).toLocaleTimeString()}<br/>
       Current state:<br/>
